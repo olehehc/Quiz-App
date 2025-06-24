2470cfab437cfb1c4435d157c7fb99e6865a5a59
Oleh Shpoltakov <ggsubsy@gmail.com>
2025-06-24 19:09:40 +0200

    Adding a Quiz component

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
+
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
+  }
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <p>{QUESTIONS[activeQuestionIndex].text}</p>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
